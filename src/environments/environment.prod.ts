836c60ce46e9863877db36961d1a53c89ddda756
Gary Ayala <gary.ayala.rojas@gmail.com>
2020-11-16 18:09:36 -0500

    remove access token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  accessToken: '86bb6f18f0f1518c8ad28f685dd24c1e1673e04b'
+  accessToken: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  accessToken: '86bb6f18f0f1518c8ad28f685dd24c1e1673e04b'
+  accessToken: ''
 };
 
 /*
