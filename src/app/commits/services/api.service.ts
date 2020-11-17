4a65a153328dde0336aca6cb7c93401ff98eafa5
Gary Ayala <gary.ayala.rojas@gmail.com>
2020-11-16 21:01:44 -0500

    Remove auth token

--- src/app/commits/services/api.service.ts
+++ src/app/commits/services/api.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,30 +7,16 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   private API_BASE: string;
-  private ACCESS_TOKEN: string;
 
   constructor(private http: HttpClient) {
     this.API_BASE = 'https://api.github.com/repos/garyayala/fulltimeforce-task';
-    this.ACCESS_TOKEN = environment.accessToken;
-  }
-
-  getUserInfo(): Observable<any>{
-    return this.http.get<any>(`https://github.com/user`, {
-      headers: new HttpHeaders().set('Authorization', `Bearer ${this.ACCESS_TOKEN}`)
-    });
   }
 
   getList(): Observable<any>{
-    return this.http.get<any>(`${this.API_BASE}/commits`, {
-      headers: new HttpHeaders().set('Authorization', `Bearer ${this.ACCESS_TOKEN}`)
-                                .set('Accept', `application/vnd.github.cloak-preview+json`)
-    });
+    return this.http.get<any>(`${this.API_BASE}/commits`);
   }
 
   get(sha: string): Observable<any>{
-    return this.http.get<any>(`${this.API_BASE}/commits/${sha}`, {
-      headers: new HttpHeaders().set('Authorization', `Bearer ${this.ACCESS_TOKEN}`)
-                                .set('Accept', `application/vnd.github.cloak-preview+json`)
-    });
+    return this.http.get<any>(`${this.API_BASE}/commits/${sha}`);
   }
 }
